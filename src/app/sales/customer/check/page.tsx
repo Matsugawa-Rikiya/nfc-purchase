49e1066d13c05f606472376087b14ab7598f69f9
Matsugawa-Rikiya <m-rikiya-2412@occ.co.jp>
2024-09-04 18:18:10 +0900

    どうせ無理だろ

--- src/app/sales/customer/check/page.tsx
+++ src/app/sales/customer/check/page.tsx
@@ -1,8 +1,8 @@
+// CheckPage.tsx
 "use client";
 
-import dynamic from 'next/dynamic';
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import {
   Card,
   CardContent,
@@ -16,15 +16,11 @@ import { getIDmStr } from "@/app/lib/nfc/rcs300.mjs";
 import { useToast } from "@/components/ui/use-toast";
 import { putSoldSeparately, getUserByNfcId } from "@/app/sql/sqls";
 
-// サスペンスでラップする関数のみに焦点を当てる
+// コンポーネントの定義
 const CheckPage = () => {
+  const searchParams = useSearchParams();
   const router = useRouter();
-  const searchParams = useSearchParams(); // useSearchParams の呼び出し
-  const CheckPage = dynamic(() => import('./page'), {
-    ssr: false,  // サーバーサイドレンダリングを無効にする
-  });
 
-  // サスペンスでラップする内容
   const name = searchParams.get("name");
   const ticket = searchParams.get("ticket");
 
@@ -129,94 +125,96 @@ const CheckPage = () => {
   };
 
   return (
-    <div className="flex w-full p-10 space-x-5">
-      <div className="flex flex-col w-1/2 items-center border-2 p-5">
-        <h1 className="text-5xl font-bold">
-          {!isConfirmed ? "注文内容" : "領収書"}
-        </h1>
-        <hr className="w-full border-t-2 border-gray-300 mt-4" />
-        <div className="text-4xl mt-10 w-full">
-          <p className="text-right">{today}</p>
-          <div className="flex w-full">
-            <div className="flex flex-col w-1/3 text-right space-y-10">
-              <p>お名前:</p>
-              <p>販売者ID:</p>
-              <p>販売者名:</p>
-            </div>
-            <div className="flex flex-col w-2/3 text-left space-y-10">
-              <p className="pl-10 font-bold">{name}</p>
-              {isAdmin && employeeName && employeeId && (
-                <div className="pl-10 font-bold flex flex-col space-y-10">
-                  <p>{employeeId}</p>
-                  <p>{employeeName}</p>
-                </div>
-              )}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex w-full p-10 space-x-5">
+        <div className="flex flex-col w-1/2 items-center border-2 p-5">
+          <h1 className="text-5xl font-bold">
+            {!isConfirmed ? "注文内容" : "領収書"}
+          </h1>
+          <hr className="w-full border-t-2 border-gray-300 mt-4" />
+          <div className="text-4xl mt-10 w-full">
+            <p className="text-right">{today}</p>
+            <div className="flex w-full">
+              <div className="flex flex-col w-1/3 text-right space-y-10">
+                <p>お名前:</p>
+                <p>販売者ID:</p>
+                <p>販売者名:</p>
+              </div>
+              <div className="flex flex-col w-2/3 text-left space-y-10">
+                <p className="pl-10 font-bold">{name}</p>
+                {isAdmin && employeeName && employeeId && (
+                  <div className="pl-10 font-bold flex flex-col space-y-10">
+                    <p>{employeeId}</p>
+                    <p>{employeeName}</p>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
+          <table className="table-auto w-full mt-10 text-3xl">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">アイテム</th>
+                <th className="px-4 py-2">単価</th>
+                <th className="px-4 py-2">数量</th>
+                <th className="px-4 py-2">小計</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border px-4 py-2">チケット</td>
+                <td className="border px-4 py-2">
+                  {ticketPrice.toLocaleString()}円
+                </td>
+                <td className="border px-4 py-2">{ticket}枚</td>
+                <td className="border px-4 py-2">
+                  {ticketSubtotal.toLocaleString()}円
+                </td>
+              </tr>
+            </tbody>
+            <tfoot className="text-4xl font-semibold bg-gray-100">
+              <tr>
+                <td className="border px-4 py-2 text-right" colSpan={3}>
+                  合計金額
+                </td>
+                <td className="border px-4 py-2">
+                  {totalAmount.toLocaleString()}円
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          <div className="flex w-full items-center justify-center space-x-10 mt-20">
+            {isConfirmed && isAdmin && (
+              <OKButton className="w-1/2 py-10 text-2xl" onClick={handleOK}>
+                OK
+              </OKButton>
+            )}
+            <CancelButton
+              className="w-1/2 py-10 text-2xl"
+              onClick={handleCancel}
+            >
+              Cancel
+            </CancelButton>
+          </div>
         </div>
-        <table className="table-auto w-full mt-10 text-3xl">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">アイテム</th>
-              <th className="px-4 py-2">単価</th>
-              <th className="px-4 py-2">数量</th>
-              <th className="px-4 py-2">小計</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border px-4 py-2">チケット</td>
-              <td className="border px-4 py-2">
-                {ticketPrice.toLocaleString()}円
-              </td>
-              <td className="border px-4 py-2">{ticket}枚</td>
-              <td className="border px-4 py-2">
-                {ticketSubtotal.toLocaleString()}円
-              </td>
-            </tr>
-          </tbody>
-          <tfoot className="text-4xl font-semibold bg-gray-100">
-            <tr>
-              <td className="border px-4 py-2 text-right" colSpan={3}>
-                合計金額
-              </td>
-              <td className="border px-4 py-2">
-                {totalAmount.toLocaleString()}円
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-        <div className="flex w-full items-center justify-center space-x-10 mt-20">
-          {isConfirmed && isAdmin && (
-            <OKButton className="w-1/2 py-10 text-2xl" onClick={handleOK}>
-              OK
-            </OKButton>
-          )}
-          <CancelButton
-            className="w-1/2 py-10 text-2xl"
-            onClick={handleCancel}
-          >
-            Cancel
-          </CancelButton>
+        <div className="flex flex-col w-1/2 items-center border-2 p-5">
+          <h1 className="text-5xl font-bold">販売担当</h1>
+          <Card className="w-96 mx-auto mt-10">
+            <CardHeader>
+              <CardTitle className="text-center">Confirmation</CardTitle>
+              <CardDescription className="text-center text-sm">
+                NFCカードをかざしてください
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex justify-center border-2 rounded-lg py-10">
+                <HiOutlineIdentification className="text-9xl" />
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </div>
-      <div className="flex flex-col w-1/2 items-center border-2 p-5">
-        <h1 className="text-5xl font-bold">販売担当</h1>
-        <Card className="w-96 mx-auto mt-10">
-          <CardHeader>
-            <CardTitle className="text-center">Confirmation</CardTitle>
-            <CardDescription className="text-center text-sm">
-              NFCカードをかざしてください
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex justify-center border-2 rounded-lg py-10">
-              <HiOutlineIdentification className="text-9xl" />
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
